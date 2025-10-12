bfe0e7f46b18ac780445c2aeea1411afe8574301
Varezhka69 <vareckakuznecova@gmail.com>
2025-10-12 20:07:21 +0300

    Update RolesForm.cs

--- Src/RolesForm.cs
+++ Src/RolesForm.cs
@@ -2,7 +2,7 @@ using System;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace SmartphoneDefectsDatabase
+namespace SmartphoneDefectsDatabase1
 {
     public partial class RolesForm : Form
     {
